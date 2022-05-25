017e6b3c1379fb8af5582e44db03206127823a7e
vr.afons <vitorafonsosilvaoliveira@gmail.com>
2022-05-24 23:37:20 -0400

    hdh

--- sever.js
+++ sever.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 6060;
 
 
 /*app.get('/', function(req, res) {
@@ -13,7 +13,7 @@ app.use('/perfil01',express.static(__dirname + '/assets'));
 
 app.use('/logintoken',express.static(__dirname + '/loginuser'));
 
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + '/play'));
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
