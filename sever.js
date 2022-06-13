b3d0899ecd13b39e3c254273d695fd64ea62d386
vitorafonsosilvaoliveira@gmail.com <vitorafonsosilvaoliveira@gmail.com>
2022-06-12 23:03:43 -0400

    h

--- sever.js
+++ sever.js
@@ -7,12 +7,39 @@ const port = process.env.PORT || 6060;
 app.use('/static', express.static('public'));
 
 
+
+
 app.use('/all',express.static(__dirname + '/assets/all'));
-app.use('/serie01',express.static(__dirname + '/assets/Agenteof'));
-app.use('/serie02',express.static(__dirname + '/assets/the'));
+
+
+
+app.use('/got',express.static(__dirname + '/assets/got'));
+app.use('/temp1',express.static(__dirname + '/assets/got/temps1'));
+app.use('/temp2',express.static(__dirname + '/assets/got/temps2'));
+app.use('/temp3',express.static(__dirname + '/assets/got/temps3'));
+app.use('/temp4',express.static(__dirname + '/assets/got/temps4'));
+app.use('/temp5',express.static(__dirname + '/assets/got/temps5'));
+app.use('/temp6',express.static(__dirname + '/assets/got/temps6'));
+app.use('/temp7',express.static(__dirname + '/assets/got/temps7'));
+app.use('/temp8',express.static(__dirname + '/assets/got/temps8'));
+
+
+
+
+
+
+
+
 app.use('/perfil01',express.static(__dirname + '/assets'));
 
+
+
+
+
 app.use('/logintoken',express.static(__dirname + '/loginuser'));
+
+
+
 
 app.use(express.static(__dirname + '/login'));
 
